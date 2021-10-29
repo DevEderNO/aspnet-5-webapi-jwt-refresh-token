50e776f0361f1bcc2cba32d2367729d1f4262cf1
DevEderNO <sk9eder@gmail.com>
2021-10-29 00:39:51 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using RefreshTokenAuth.Models;
+using RefreshTokenAuth.Repositories;
+using RefreshTokenAuth.Services;
+
+namespace RefreshTokenAuth.Controllers
+{
+  [ApiController]
+  public class LoginController : ControllerBase
+  {
+    [HttpPost]
+    [Route("login")]
+    public ActionResult<dynamic> Authenticate([FromBody] User model)
+    {
+      var user = UserRepository.Get(model.Username, model.Password);
+      if (user == null)
+        return NotFound(new { message = "Invalid username or password" });
+      var token = TokenService.GenerateToken(user);
+      var refreshToken = TokenService.GetRefreshToken(token);
+      TokenService.SaveRefreshToken(user.Username, refreshToken);
+      user.Password = "";
+      return new
+      {
+        user,
+        token,
+        refreshToken
+      };
+    }
+
+    public partial class RefreshTO
+    {
+      public string Token { get; set; }
+      public string RefreshToken { get; set; }
+    }
+
+    [HttpPost]
+    [Route("refresh")]
+    public IActionResult Refresh([FromBody] RefreshTO refreshTO)
+    {
+      var principal = TokenService.GetPrincipalFromExpiredToken(refreshTO.Token);
+      var username = principal.Identity.Name;
+      var savedRefreshToken = TokenService.GetRefreshToken(username);
+      if (savedRefreshToken != refreshTO.RefreshToken)
+        throw new SecurityTokenException("Invalid refresh token");
+
+      var newJwtToken = TokenService.GenerateToken(principal.Claims);
+      var newRefreshToken = TokenService.GenerateRefrashToken();
+      TokenService.DeleteRefreshToken(username, savedRefreshToken);
+      TokenService.SaveRefreshToken(username, newRefreshToken);
+      return new ObjectResult(new
+      {
+        token = newJwtToken,
+        refreshToken = newRefreshToken
+      });
+    }
+  }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using RefreshTokenAuth.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RefreshTokenAuth.Repositories
+{
+  public static class UserRepository
+  {
+    public static User Get(string username, string password)
+    {
+      var users = new List<User>()
+      {
+        new User{ Id = 1, Username = "batman", Password = "batman", Role = "manager"},
+        new User{ Id = 2, Username = "robin", Password = "robin", Role = "employee"},
+      };
+      return users.FirstOrDefault(x => x.Username.ToLower() == username.ToLower() && x.Password == password);
+    }
+  }
+}
